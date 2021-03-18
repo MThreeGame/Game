dc1fd56a9b7a32c02a5c07e3704e6575593792e7
Samaneh Shirdel <sam_pm17@yahoo.com>
2021-03-18 12:21:07 -0400

    update Level.cpp

--- winWindow.cpp
+++ winWindow.cpp
@@ -1,5 +1,78 @@
 //
 // Created by admin on 2021-03-18.
 //
-
 #include "winWindow.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool winWindow::init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "Result", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+            if( gRenderer == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+            }
+
+        }
+    }
+
+    return success;
+}
+
+SDL_Surface* winWindow::loadSurface( std::string path )
+{
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error\n", path.c_str() );
+        return NULL;
+    }
+    return loadedSurface;
+}
+
+void winWindow::close()
+{
+    //Destroy window
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
+
+
--- winWindow.h
+++ winWindow.h
@@ -1,14 +1,36 @@
 //
 // Created by admin on 2021-03-18.
 //
-
 #ifndef GAME_WINWINDOW_H
 #define GAME_WINWINDOW_H
-
-
+#include "SDL.h"
+#include <string>
+#include "KeyPressSurface.h"
+using namespace std;
 class winWindow {
+   public:
+    winWindow(){}
+    //Starts up SDL and creates window
+    bool init();
+    //Loads media : load all the images for the different object represenations
+    bool loadMedia();
+    //Frees media and shuts down SDL
+    void close();
+    //Loads individual image as SDL_Surface. Not used anymore.
+    SDL_Surface* loadSurface(string path);
+
+
+    //void blitSurface();
+   private:
+    // ATTRIBUTES
+    // size of the window,
+    int SCREEN_WIDTH = 500;
+    int SCREEN_HEIGHT = 400;
+    //The window we'll be rendering to
+    SDL_Window* gWindow = NULL;
+    //The window renderer
+    SDL_Renderer* gRenderer = NULL;
 
 };
-
 
 #endif //GAME_WINWINDOW_H
